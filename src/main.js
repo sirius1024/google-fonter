f6b234929008bef713d482daecd58c025cb11840
Sirius Guo <cealer@foxmail.com>
2017-06-03 15:54:39 +0800

    fixed dev option
    fix development option and default value.

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ function createWindow() {
         slashes: true
     }))
 
-    win.webContents.openDevTools();
+    //win.webContents.openDevTools();
 
     win.on('closed', () => {
         win = null;
